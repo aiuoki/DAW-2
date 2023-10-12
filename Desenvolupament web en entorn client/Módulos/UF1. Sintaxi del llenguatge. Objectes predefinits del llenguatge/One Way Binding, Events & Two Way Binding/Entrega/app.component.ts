1ebd15717a6cea072dc9e47745918e0b8d8b2360
aiuoki <118086754+aiuoki@users.noreply.github.com>
2023-10-12 14:11:15 +0200

    Update app.component.ts

--- Desenvolupament web en entorn client/Módulos/UF1. Sintaxi del llenguatge. Objectes predefinits del llenguatge/One Way Binding, Events & Two Way Binding/Entrega/app.component.ts
+++ Desenvolupament web en entorn client/Módulos/UF1. Sintaxi del llenguatge. Objectes predefinits del llenguatge/One Way Binding, Events & Two Way Binding/Entrega/app.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre?: string;
+  fuerza?: number;
+  destreza?: number;
+  inteligencia?: number;
+  constitucion?: number;
+}
+
+let fichaPersonaje: Personaje = {}
+let personajeGuardado: Personaje = {}
+
+let nombres: string[] = ['Ernesto','Calisto','Lorenzo','Alexis','Adolfo','Gabriel','Fidel','Emiliano','Alfonso','Juan Antonio',
+  'Margarita','Francisca','Iraida','Rut','Alondra','Maricarmen','Mireya','Aroa','Estela','Octavia','Luna'];
+
+function numAleatorio(): number {
+  return Math.floor(Math.random()*21);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -26,22 +44,4 @@ export class AppComponent {
   protected readonly fichaPersonaje = fichaPersonaje;
   protected readonly personajeGuardado = personajeGuardado;
 
-}
-
-interface Personaje {
-  nombre?: string;
-  fuerza?: number;
-  destreza?: number;
-  inteligencia?: number;
-  constitucion?: number;
-}
-
-let fichaPersonaje: Personaje = {}
-let personajeGuardado: Personaje = {}
-
-let nombres: string[] = ['Ernesto','Calisto','Lorenzo','Alexis','Adolfo','Gabriel','Fidel','Emiliano','Alfonso','Juan Antonio',
-  'Margarita','Francisca','Iraida','Rut','Alondra','Maricarmen','Mireya','Aroa','Estela','Octavia','Luna'];
-
-function numAleatorio(): number {
-  return Math.floor(Math.random()*21);
 }
