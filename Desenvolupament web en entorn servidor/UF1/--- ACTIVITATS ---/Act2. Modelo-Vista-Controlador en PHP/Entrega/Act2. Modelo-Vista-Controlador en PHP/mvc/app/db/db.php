058b7d792633d72ecf69ffa369def037fe2fef4a
aiuoki <118086754+aiuoki@users.noreply.github.com>
2023-11-14 19:35:01 +0100

    Delete Desenvolupament web en entorn servidor/UF1/--- ACTIVITATS ---/Act2. Modelo-Vista-Controlador en PHP/Entrega/Act2. Modelo-Vista-Controlador en PHP directory

--- Desenvolupament web en entorn servidor/UF1/--- ACTIVITATS ---/Act2. Modelo-Vista-Controlador en PHP/Entrega/Act2. Modelo-Vista-Controlador en PHP/mvc/app/db/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Conectar{
-    public static function conexion(){
-        $conexion=new mysqli("localhost", "root", "", "mvc");
-        $conexion->query("SET NAMES 'utf8'");
-        return $conexion;
-    }
-}
-?>
