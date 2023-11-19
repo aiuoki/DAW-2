f82acea4945bc41b465a1e5635f04173ca85a582
aiuoki <118086754+aiuoki@users.noreply.github.com>
2023-11-19 21:31:45 +0100

    Delete Desenvolupament web en entorn servidor/UF1/--- ACTIVITATS ---/ Act3. La tienda/Entrega/ Act3. La tienda directory

--- Desenvolupament web en entorn servidor/UF1/--- ACTIVITATS ---/ Act3. La tienda/Entrega/ Act3. La tienda/Act3. La tienda/tienda/app/db/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Conectar{
-    public static function conexion(){
-        $conexion=new mysqli("localhost", "root", "", "tienda");
-        $conexion->query("SET NAMES 'utf8'");
-        return $conexion;
-    }
-}
-?>
